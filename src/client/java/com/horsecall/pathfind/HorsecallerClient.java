30b4350744c6f4b7d1f26f05f04f43f6885cb6d4
Vaugtro <victor.agustgm@gmail.com>
2024-02-06 18:44:32 -0300

    rebase: change template version

--- src/client/java/com/horsecall/pathfind/HorsecallerClient.java
+++ src/client/java/com/horsecall/pathfind/HorsecallerClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package com.horsecall.pathfind;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class HorsecallerClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/com/horsecall/pathfind/mixin/ExampleMixin.java
+++ src/main/java/com/horsecall/pathfind/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.horsecall.pathfind.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
